4ac2a80f84d57a0bc4f4c3c3ab8926178d082022
Adryell William <adrelwilion@gmail.com>
2025-07-30 20:45:10 -0300

    Update EmendaDeComissao.java

--- ATIVIDADE EMENDA PARLAMENTAR/EmendaDeComissao.java
+++ ATIVIDADE EMENDA PARLAMENTAR/EmendaDeComissao.java
@@ -1,12 +1,18 @@
 import java.util.List;
+import java.util.HashMap;
 
-public class EmendaDeComissao {
+public class EmendaDeComissao extends Emenda {
     private String linkComissao;
     private List<String> parlamentaresComissao;
     private String linkRelatorio;
     private String parlamentarSugestor;
     
-    public EmendaDeComissao(String linkComissao, List<String> parlamentaresComissao, String linkRelatorio, String parlamentarSugestor) {
+    public EmendaDeComissao(int id, String autor, double valorTotal, int ano, String tipo, 
+                           List<Documento> documentos, HashMap<String, String> convenios, 
+                           HashMap<String, String> acordos,
+                           String linkComissao, List<String> parlamentaresComissao, 
+                           String linkRelatorio, String parlamentarSugestor) {
+        super(id, autor, valorTotal, ano, tipo, documentos, convenios, acordos);
         this.linkComissao = linkComissao;
         this.parlamentaresComissao = parlamentaresComissao;
         this.linkRelatorio = linkRelatorio;
@@ -53,7 +59,7 @@ public class EmendaDeComissao {
     
     @Override
     public String toString() {
-        return String.format("EmendaDeComissao{linkComissao='%s', parlamentaresComissao=%s, linkRelatorio='%s', parlamentarSugestor='%s'}", 
-                           linkComissao, parlamentaresComissao, linkRelatorio, parlamentarSugestor);
+        return String.format("EmendaDeComissao{id=%d, autor='%s', valorTotal=%.2f, ano=%d, tipo='%s', linkComissao='%s', parlamentaresComissao=%s, linkRelatorio='%s', parlamentarSugestor='%s', documentos=%s, convenios=%s, acordos=%s}", 
+                           id, autor, valorTotal, ano, tipo, linkComissao, parlamentaresComissao, linkRelatorio, parlamentarSugestor, documentos, convenios, acordos);
     }
 }
