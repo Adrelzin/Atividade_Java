54eb20af4d479e616325543d5352cc947d9efeb0
Adryell William <adrelwilion@gmail.com>
2025-07-30 20:45:58 -0300

    Update EmendaDeRelator.java

--- ATIVIDADE EMENDA PARLAMENTAR/EmendaDeRelator.java
+++ ATIVIDADE EMENDA PARLAMENTAR/EmendaDeRelator.java
@@ -1,8 +1,15 @@
-public class EmendaDeRelator {
+import java.util.List;
+import java.util.HashMap;
+
+public class EmendaDeRelator extends Emenda {
     private String parlamentarAutor;
     private String nomeRelator;
     
-    public EmendaDeRelator(String parlamentarAutor, String nomeRelator) {
+    public EmendaDeRelator(int id, String autor, double valorTotal, int ano, String tipo, 
+                          List<Documento> documentos, HashMap<String, String> convenios, 
+                          HashMap<String, String> acordos,
+                          String parlamentarAutor, String nomeRelator) {
+        super(id, autor, valorTotal, ano, tipo, documentos, convenios, acordos);
         this.parlamentarAutor = parlamentarAutor;
         this.nomeRelator = nomeRelator;
     }
@@ -25,6 +32,7 @@ public class EmendaDeRelator {
     
     @Override
     public String toString() {
-        return String.format("EmendaDeRelator{parlamentarAutor='%s', nomeRelator='%s'}", parlamentarAutor, nomeRelator);
+        return String.format("EmendaDeRelator{id=%d, autor='%s', valorTotal=%.2f, ano=%d, tipo='%s', parlamentarAutor='%s', nomeRelator='%s', documentos=%s, convenios=%s, acordos=%s}", 
+                           id, autor, valorTotal, ano, tipo, parlamentarAutor, nomeRelator, documentos, convenios, acordos);
     }
 }
